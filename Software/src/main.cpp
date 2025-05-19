07eeb0eac94bc0dfa3571dbdbee1675700dd486b
lawnon <nonsobobanyeji@gmail.com>
2025-05-19 03:02:33 +0200

    testing stuff

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -2,17 +2,21 @@
 
 // put function declarations here:
 int myFunction(int, int);
+int result;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.write("Hello World");
+  Serial.write(result);
   delay(1000);
+  Serial.write("MyFunction Result");
+  result = myFunction(2, 3);
+  Serial.write(result);
 }
 
 // put function definitions here:
