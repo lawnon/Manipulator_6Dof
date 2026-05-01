39e42da4dd3eb092d94bb82ad8aaecf91ac079f7
Lawnon <nonsobobanyeji@gmail.com>
2026-05-01 18:45:23 +0200

    Update to Clang compiler path

--- Software/Ctrl/code/build.sh
+++ Software/Ctrl/code/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Jesus is Lord !!"
 
-/usr/bin/clang++-22 -std=c++20 -O2 -Wall  -o ./build/ctrl.o -c ./code/ctrl.cpp
+clang++-22 -std=c++20 -O2 -Wall  -o ./build/ctrl.o -c ./code/ctrl.cpp
 
